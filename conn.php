7e71ec9203d503832388dfc8a7545dc5fe38f413
WilliamEXP <william.17070017@itsmotul.edu.mx>
2020-12-03 16:35:38 -0600

    .

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost", "root","", "carpentry_project");
+$conexion = mysqli_connect("localhost", "root","", "carpentry_proyect");
 if (!$conexion)
 {
     echo 'Error al conectar a la base de datos';
