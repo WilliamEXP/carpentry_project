051746245e14b8e2db350db29168f0ca5d68f623
WilliamEXP <william.17070017@itsmotul.edu.mx>
2020-12-04 02:20:11 -0600

    prubea

---
+++ conexiones/insertar.js
@@ -0,0 +1,25 @@
+function insertar(){
+
+    <?php
+
+    include 'conn.php';
+    $nombre = $_POST["nombre"];
+    $cantidad = $_POST["cantidad"];
+    $precio = $_POST["precio_sugerido"];
+    $tipo = $_POST["tipo"];
+    
+    $insertar = "INSERT INTO producto (nombre, tipo,precio, cantidad) VALUES ('$nombre','$tipo','$precio', '$cantidad')";
+    $resultado = mysqli_query($conexion, $insertar);
+    if (!$resultado)
+    {
+        echo 'Error';
+    }
+    else
+    {
+    echo '<script>
+    window.history.go(-1);
+    alert("Agregado")
+    </script>';    
+    }
+    mysqli_close($conexion);?>
+  };
